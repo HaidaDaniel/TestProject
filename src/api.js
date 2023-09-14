14cc30d6c202111a1e1f8b4ef1c8d2ed84d72cd4
Daniel Haida <danilgajda22@gmail.com>
2023-09-14 10:21:43 +0300

    add dot env file

--- src/api.js
+++ src/api.js
@@ -1,6 +1,11 @@
+
+const PRODUCTS_URL = process.env.REACT_APP_PRODUCTS_URL
+const PRODUCTS_URL_BY_ID = process.env.REACT_APP_PRODUCT_BY_ID_URL
+
+
 export const fetchProducts = async () => {
     try {
-        const response = await fetch('http://demo5127360.mockable.io/products');
+        const response = await fetch(PRODUCTS_URL);
         if (!response.ok) {
             throw new Error('Response is not OK');
         }
@@ -14,7 +19,7 @@ export const fetchProducts = async () => {
 
 export const fetchProductById = async (id) => {
     try {
-        const response = await fetch(`http://demo5127360.mockable.io/product/${id}`);
+        const response = await fetch(PRODUCTS_URL_BY_ID + id);
         if (!response.ok) {
             throw new Error('Response is not OK');
         }
