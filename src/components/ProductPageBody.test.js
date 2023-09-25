731247e08165ca49cb8ec6b168fccd07dc584227
Daniel Haida <danilgajda22@gmail.com>
2023-09-25 10:22:33 +0300

    small fix

--- src/components/ProductPageBody.test.js
+++ src/components/ProductPageBody.test.js
@@ -1,6 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
 import '@testing-library/jest-dom/extend-expect';
 
 import ProductPageBody from './ProductPageBody';
