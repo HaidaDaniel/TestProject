24e89caa874d1443250f25a27725fa643157f3e6
Daniel Haida <danilgajda22@gmail.com>
2023-09-27 14:58:59 +0300

    removed snapshots to separate dir

--- snapshotResolver.js
+++ snapshotResolver.js
@@ -3,14 +3,12 @@ module.exports = {
 
     resolveSnapshotPath: (testPath, snapshotExtension) => {
         const newSnapPath = testPath.replace('tests\\', 'tests\\__snapshots__\\').replace('.test', '.snap')
-        console.log(newSnapPath)
-        return (testPath.replace(/\.test\.([tj]sx?)/, `${snapshotExtension}.$1`))
+        return newSnapPath
     },
 
     resolveTestPath: (snapshotFilePath, snapshotExtension) => {
-        const newTestPath = snapshotFilePath.replace(snapshotExtension, '.test')
-        console.log(newTestPath + 'ttt')
-        return snapshotFilePath.replace(snapshotExtension, '.test')
+        const newTestPath = snapshotFilePath.replace('tests\\__snapshots__\\', 'tests\\').replace('.snap', '.test')
+        return newTestPath
     }
     ,
 }
